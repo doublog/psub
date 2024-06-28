1f7492df78a3d01abb677a40b185d4538a333efd
jiangweiye <jiangwy@staryea.com>
2024-06-28 16:16:54 +0800

    update

--- worker.js
+++ worker.js
@@ -2918,7 +2918,7 @@ var src_default = {
   async fetch(request, env) {
     const url = new URL(request.url);
     const host = url.origin;
-    const frontendUrl = env.FRONTEND || 'https://raw.githubusercontent.com/jwyGithub/subconverter-cloudflare/main/index.html';
+    const frontendUrl = env.FRONTEND || 'https://raw.githubusercontent.com/bulianglin/psub/main/frontend.html';
     const SUB_BUCKET = env.SUB_BUCKET;
     const REMOTE_CONFIG = env.REMOTE_CONFIG;
     const remoteConfig = getRemoteConfig(REMOTE_CONFIG);
